7041b941ff53b1613b49c7159325ac541882eea3
VladPodilnyk <vladyslav.podilnyk@bolt.eu>
2023-05-06 23:32:26 +0300

    rework structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,92 +1,27 @@
 import { Header } from './components/Header';
-import { useCallback, useEffect, useState } from 'react';
-import { IMAGES, randomCards, shuffle } from './utils/shuffle';
-import { CardComponent } from './components/Card';
-import { Card } from './domain/Card';
+import { useEffect } from 'react';
+import { CardComponent } from './components/CardComponent';
+import { useGameState } from './hooks/useGameState';
 
 export const App: React.FC = () => {
-  const [cards, setCards] = useState(randomCards);
-
-  // our two consecutive choices
-  const [pickOne, setPickOne] = useState<Card>();
-  const [pickTwo, setPickTwo] = useState<Card>();
-
-  const [wins, setWins] = useState(0);
-
-  // prevent clicking on cards during animation
-  const [disabled, setDisabled] = useState(false);
-
-  // shuffling images, but not cards, so we won't regenerate index and avoid re-rendering
-  const shuffleCards = useCallback(() => {
-    const randomImages = shuffle(IMAGES);
-    setCards(cards.map((card, index) => ({ ...card, image: randomImages[index], matched: false })));
-  }, [cards]);
-
-  const handleCardClick = useCallback((card: Card) => {
-    return () => {
-      if (!disabled) {
-        pickOne ? setPickTwo(card) : setPickOne(card);
-      }
-    };
-  }, [pickOne, disabled]);
-
-  const handleTurn = useCallback(() => {
-    setPickOne(undefined);
-    setPickTwo(undefined);
-    setDisabled(false);
-  }, []);
-
-  const onNewGame = useCallback(() => {
-    handleTurn();
-    setCards(shuffle);
-    setWins(0);
-  }, [handleTurn]);
+  const {
+    cards,
+    pickOne,
+    pickTwo,
+    wins,
+    handleCardClick,
+    onNewGame,
+    updateCardsAndSetTimeout,
+    updateWins,
+  } = useGameState();
 
   useEffect(() => {
-    let pickTimer: number;
-    if (pickOne && pickTwo) {
-      if (pickOne.image === pickTwo.image) {
-        setCards((state) => {
-          return state.map((card) => {
-            if (card.image === pickOne.image) {
-              return {
-                ...card,
-                matched: true,
-              };
-            }
-            return card;
-          });
-        }
-        );
-        handleTurn();
-      } else {
-        setDisabled(true);
-        pickTimer = setTimeout(() => {
-          handleTurn();
-        }, 1000);
-      }
-
-      return () => {
-        clearTimeout(pickTimer);
-      };
-    }
-  }, [pickOne, pickTwo, cards, wins, handleTurn]);
+    return updateCardsAndSetTimeout();
+  }, [updateCardsAndSetTimeout]);
 
   useEffect(() => {
-    let timer: number;
-    const matchedCards = cards.filter((card) => !card?.matched);
-    if (matchedCards.length === 0) {
-      console.log('You win!');
-      setWins((wins) => wins + 1);
-      timer = setTimeout(() => {
-        handleTurn();
-        shuffleCards();
-      }, 1000);
-      return () => {
-        clearTimeout(timer);
-      };
-    }
-  }, [cards, handleTurn, shuffleCards]);
+    return updateWins();
+  }, [updateWins]);
 
   return (
     <div>
---
+++ src/hooks/useGameState.ts
@@ -0,0 +1,101 @@
+import { useCallback, useState } from 'react';
+import { IMAGES, randomCards, shuffle } from '../utils/shuffle';
+import { Card } from '../domain/Card';
+
+const updateMatchedImages = (cards: Card[], image: string) => {
+  return cards.map((card) => {
+    if (card.image === image) {
+      return {
+        ...card,
+        matched: true,
+      };
+    }
+    return card;
+  });
+};
+
+// ms
+const TIMEOUT = 1000;
+
+export const useGameState = () => {
+  const [cards, setCards] = useState(randomCards);
+
+  // our two consecutive choices
+  const [pickOne, setPickOne] = useState<Card>();
+  const [pickTwo, setPickTwo] = useState<Card>();
+
+  const [wins, setWins] = useState(0);
+
+  // prevent clicking on cards during animation
+  const [disabled, setDisabled] = useState(false);
+
+  // shuffling images, but not cards, so we won't regenerate index and avoid re-rendering
+  const shuffleCards = useCallback(() => {
+    const randomImages = shuffle(IMAGES);
+    setCards(cards.map((card, index) => ({ ...card, image: randomImages[index], matched: false })));
+  }, [cards]);
+
+  const handleCardClick = useCallback((card: Card) => {
+    return () => {
+      if (!disabled) {
+        pickOne ? setPickTwo(card) : setPickOne(card);
+      }
+    };
+  }, [pickOne, disabled]);
+
+  const handleTurn = useCallback(() => {
+    setPickOne(undefined);
+    setPickTwo(undefined);
+    setDisabled(false);
+  }, []);
+
+  const onNewGame = useCallback(() => {
+    handleTurn();
+    setCards(shuffle);
+    setWins(0);
+  }, [handleTurn]);
+
+  const updateCardsAndSetTimeout = useCallback(() => {
+    let pickTimer: number;
+    if (pickOne && pickTwo) {
+      if (pickOne.image === pickTwo.image) {
+        setCards((cards) => updateMatchedImages(cards, pickOne.image));
+        handleTurn();
+      } else {
+        setDisabled(true);
+        pickTimer = setTimeout(() => {
+          handleTurn();
+        }, TIMEOUT);
+      }
+    }
+    return () => clearTimeout(pickTimer);
+  }, [handleTurn, pickOne, pickTwo]);
+
+  const updateWins = useCallback(() => {
+    let timer: number;
+    const matchedCards = cards.filter((card) => !card?.matched);
+    if (matchedCards.length === 0) {
+      console.log('You win!');
+      setWins((wins) => wins + 1);
+      timer = setTimeout(() => {
+        handleTurn();
+        shuffleCards();
+      }, TIMEOUT);
+    }
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cards, handleTurn, shuffleCards]);
+
+  return {
+    cards,
+    pickOne,
+    pickTwo,
+    wins,
+    handleCardClick,
+    onNewGame,
+    updateCardsAndSetTimeout,
+    updateWins,
+  };
+};
